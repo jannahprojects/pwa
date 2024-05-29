87002cab72cebd434ae52c0a4b370b81d2cd1771
jannahprojects <kims43846@gmail.com>
2024-05-30 00:53:34 +0700

    pwa

---
+++ wp-content/uploads/uag-plugin/assets/1000/uag-js-1488.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-a45b8db4' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-8bb4b826' );
+});
+ });
